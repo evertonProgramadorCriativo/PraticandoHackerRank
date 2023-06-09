defb801357225d491c5d5111968030f5ca7d3386
evertonprogramadorcriativo <evertonprogramadorcriativo@gmail.com>
2023-06-08 23:30:29 -0300

    Time Conversion full

--- Time Conversion/index.js
+++ Time Conversion/index.js
@@ -1,10 +1,20 @@
 function timeConversion(s) {
-    let amPm = s.chartAt(8);
+    let amPm = s.charAt(8);
     let military = "";
-    if (amPm === "A") {
+    if (amPm == "A") {
         if(s.substring(0,2) == '12') {
             military = "00"
     } else {
         military = s.substring(0,2);
     }
-} }
+} 
+else {
+    //PM
+    if (s.substring(0,2) == "12") {
+        military = s.substring(0,2);
+    } else {
+        military =  parseInt(s.substring(0,2), 10) + 12; 
+    }
+}
+return military + s.substring(2,8);
+}
