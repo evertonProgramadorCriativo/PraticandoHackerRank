639b3d61e9e4555a03040bd56b2123a83a3fd662
evertonprogramadorcriativo <evertonprogramadorcriativo@gmail.com>
2023-06-09 11:18:32 -0300

    testando  o converso do horaria da manã

--- Time Conversion/index.js
+++ Time Conversion/index.js
@@ -18,3 +18,12 @@ else {
 }
 return military + s.substring(2,8);
 }
+
+//horario de manhã
+
+let  horariaMilitar = "02:01:00"
+
+let resultado1 = horariaMilitar.substring(0,2)
+let resultado2 = horariaMilitar.substring(2,8)
+
+console.log(resultado1 + resultado2)
