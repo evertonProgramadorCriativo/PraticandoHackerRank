191bd67dc0e485f066304246953961fd131aa2d3
evertonprogramadorcriativo <evertonprogramadorcriativo@gmail.com>
2023-06-08 22:54:30 -0300

    miniMaxSum Parte 01

--- miniMaxSum/index.js
+++ miniMaxSum/index.js
@@ -2,8 +2,19 @@ function miniMaxSum(arr) {
     let max = arr[0];
     let min = arr[0]
     let sum = 0;
-
-    return console.log(max)
+for (let index = 0; index < arr.length; index++) {
+   //console.log(index)
+   if (max < arr[index]) {
+    max = arr[index]
+    console.log("maximo" + " " + index)
+   }
+   if (min > arr[index]) {
+    min = arr[index]
+   console.log("minimo"  + " " + index)
+   }
+ //   sum += arr[index];
+    
 }
-
+}
+//console.log(1 > 1)
 miniMaxSum([1,3,4,6])
