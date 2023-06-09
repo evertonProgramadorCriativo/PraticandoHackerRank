58c2302dd6a872f3bb2fc6a93260eb9351e3df9c
evertonprogramadorcriativo <evertonprogramadorcriativo@gmail.com>
2023-06-09 11:44:24 -0300

    testando  o converso do horaria da tarde

--- Time Conversion/index.js
+++ Time Conversion/index.js
@@ -20,10 +20,21 @@ return military + s.substring(2,8);
 }
 
 //horario de manhã
-
-let  horariaMilitar = "13:01:00"
+/**
+let  horariaMilitar = "12:01:00"
 
 let resultado1 = horariaMilitar.substring(0,2)
 let resultado2 = horariaMilitar.substring(2,8)
 
-console.log(resultado1 + resultado2)
+console.log(resultado1 + resultado2) */
+
+//horario de tarde good afternoon 
+
+let  horariaMilitar = "07:01:00PM"
+
+let resultado1 = horariaMilitar.substring(0,2)
+ 
+
+
+ 
+console.log(parseInt(resultado1.substring(0,2), 10) + 12)
