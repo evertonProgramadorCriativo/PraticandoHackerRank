f0c6bd170ad9326b3bcd44e7022c398b0e6be10e
evertonprogramadorcriativo <evertonprogramadorcriativo@gmail.com>
2023-06-11 00:05:20 -0300

    Sparse Array parte 01

--- Sparse Arrays/index.js
+++ Sparse Arrays/index.js
@@ -1,5 +1,10 @@
 function matchingStrings(strings = [], queries = []) {
 const counter = new Map();
 
-strings.forEach()
+strings.forEach(s => {
+    if(!counter.has(s)) counter.set(s,1)
+    else counter.set(s, counter.get(s)  + 1)
+})
+return queries.map(q => counter.get(q) || 0)
+
 }
