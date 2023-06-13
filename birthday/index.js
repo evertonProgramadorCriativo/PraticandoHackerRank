d5e1f4823695c47511de67f79e7b3cd14ace79fe
evertonprogramadorcriativo <evertonprogramadorcriativo@gmail.com>
2023-06-12 23:40:25 -0300

    birthday parte 02 loop for

--- birthday/index.js
+++ birthday/index.js
@@ -2,7 +2,12 @@ function birthday(s,d,m) {
     let count = 0;
     let sum = 0 ;
 
+    for (let index = 0; index <= (s.length - m); index++) {
+     
+        
+    }
 
+    console.log(count);
 }
 
 birthday([2,2,1,3,2], 4,2)
