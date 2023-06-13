bf1495305893710034f3313fe8696edfcaf510ef
evertonprogramadorcriativo <evertonprogramadorcriativo@gmail.com>
2023-06-12 23:41:05 -0300

    birthday full

--- birthday/index.js
+++ birthday/index.js
@@ -3,11 +3,19 @@ function birthday(s,d,m) {
     let sum = 0 ;
 
     for (let index = 0; index <= (s.length - m); index++) {
-     
+        for (let index2 = 0; index2 < m; index2++) {
+            sum = sum + s[index + index2];
+           
+       }
+       if(sum === d) {
+           count += 1;
+       }
+
+       sum = 0 ;
         
     }
 
-    console.log(count);
+    return count;
 }
 
 birthday([2,2,1,3,2], 4,2)
