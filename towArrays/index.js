1f8675742e2f20c7e28a6e3b85684fec3669bd2e
evertonprogramadorcriativo <evertonprogramadorcriativo@gmail.com>
2023-06-12 23:16:36 -0300

    tow array full

--- towArrays/index.js
+++ towArrays/index.js
@@ -2,4 +2,11 @@ function towArray(K,A,B) {
     let result = "YES"
     A = A.sort((a,b) => a-b);
     B = B.sort((a,b) => b-a);
+
+    A.map((data,index) => {
+        if(A[index] + B[index] < k) {
+            return result = "NO"
+        }
+    })
+    return result;
 }
