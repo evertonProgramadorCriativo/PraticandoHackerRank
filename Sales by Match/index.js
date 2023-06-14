470e2179324cebe20e89e36b71303d4c5c08947f
evertonprogramadorcriativo <evertonprogramadorcriativo@gmail.com>
2023-06-13 22:48:29 -0300

    Sock Merchant parte 03

--- Sales by Match/index.js
+++ Sales by Match/index.js
@@ -1,5 +1,12 @@
 function sockMerchant(n, ar) {
     // Write your code here
     let pairs = 0;
-    let search = new Set()
+    let search = new Set();
+
+    for (const sock of ar) {
+        
+        
+    }
+
+    return pairs;
 }
