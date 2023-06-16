51628185a81a34287b72789e3df99f25cb2f00b8
evertonprogramadorcriativo <evertonprogramadorcriativo@gmail.com>
2023-06-15 23:52:51 -0300

    Max Min full

--- maxMin/index.py
+++ maxMin/index.py
@@ -1,6 +1,9 @@
 def maxMin(k, arr):
-   
-     
+    
     arr.sort()
     min_diff = sys.maxsize
     
+    for i in range(n-k+1):
+        min_diff =  min(min_diff, arr[i+k-1] - arr[i])
+    
+    return min_diff 
