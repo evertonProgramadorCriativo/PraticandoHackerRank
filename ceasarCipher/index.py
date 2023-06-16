1bc5cd018a0edd2059bca6ed005bbf288ed10664
evertonprogramadorcriativo <evertonprogramadorcriativo@gmail.com>
2023-06-15 23:28:23 -0300

    Caesar Cipher full

--- ceasarCipher/index.py
+++ ceasarCipher/index.py
@@ -4,3 +4,11 @@ def caesarCipher(s, k):
     
     for char in s:
         temp.append(ord(char))
+    
+    for i in range(n):
+        if 65 <= temp[i] <= 90:
+            temp[i] = (65 + (temp[i]-65+k)%26)
+        
+        elif 97 <= temp[i] <= 122:
+            temp[i] = (97 + (temp[i]-97+k)%26)
+    return "".join(map(chr,temp))
